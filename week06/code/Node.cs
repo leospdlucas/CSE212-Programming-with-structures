21d001a16993b1a4da47d6c7c30aec72d5593f19
LeoLucas <leospd.lucas@gmail.com>
2026-02-09 21:00:10 -0300

    week06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -4,15 +4,17 @@ public class Node
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data)
-    {
+    public Node(int data) {
         this.Data = data;
     }
 
-    public void Insert(int value)
-    {
+    public void Insert(int value) {
         // TODO Start Problem 1
-
+        if (value == Data)
+        {
+            return;
+        }
+        
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +36,21 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+            
+        if (value < Data) {
+            return Left != null && Left.Contains(value);
+        }else {
+            return Right != null && Right.Contains(value);
+        }
     }
 
-    public int GetHeight()
-    {
+    public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
+        //return 0; // Replace this line with the correct return statement(s)
     }
 }
